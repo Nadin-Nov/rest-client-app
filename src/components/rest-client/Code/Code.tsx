220e6643674be08784c11d802e708df61ddbaa78
Marta <emeraldraven27@gmail.com>
2025-09-11 13:42:49 +0300

    refactor: edit code tabs

--- src/components/rest-client/Code/Code.tsx
+++ src/components/rest-client/Code/Code.tsx
@@ -1,42 +1,31 @@
-import { Tabs } from '@mantine/core';
-import type { FC } from 'react';
-import { useState } from 'react';
+import { type FC, useState } from 'react';
 
-const languages: string[] = [
-  'curl',
-  'JavaScript (Fetch api)',
-  'JavaScript (XHR)',
-  'NodeJS',
-  'Python',
-  'Java',
-  'C#',
-  'Go',
-];
+import { TextArea } from '@/components/ui/TextArea/TextArea';
+
+import styles from './Code.module.css';
+
+const languages = ['curl', 'JavaScript (Fetch api)', 'JavaScript (XHR)', 'NodeJS', 'Python', 'Java', 'C#', 'Go'];
 
 export const Code: FC = () => {
-  const [language, setLanguage] = useState<string>('curl');
-
-  const handleTabChange = (value: string | null) => {
-    if (value && languages.includes(value) && value !== null) {
-      setLanguage(value);
-    }
-  };
+  const [code, setCode] = useState('');
+  const [language, setLanguage] = useState(languages[0]);
 
   return (
-    <Tabs value={language} onChange={handleTabChange} variant='outline'>
-      <Tabs.List>
-        {languages.map((el) => (
-          <Tabs.Tab key={el} value={el}>
-            {el}
-          </Tabs.Tab>
-        ))}
-      </Tabs.List>
+    <div className={styles.container}>
+      <div className={styles.tabs}>
+        {languages.map((el) => {
+          const isActive = el === language;
+          return (
+            <div key={el}>
+              <div className={`${styles.tab} ${isActive ? styles.active : ''}`} onClick={() => setLanguage(el)}>
+                {el}
+              </div>
+            </div>
+          );
+        })}
+      </div>
 
-      {languages.map((el) => (
-        <Tabs.Panel key={el} value={el}>
-          <div>code for {el}</div>
-        </Tabs.Panel>
-      ))}
-    </Tabs>
+      <TextArea placeholder={`Generated code for ${language}...`} value={code} onChange={setCode} readOnly={true} />
+    </div>
   );
 };
