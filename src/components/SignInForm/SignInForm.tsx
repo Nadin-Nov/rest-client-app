496ba9e9c03cde6f5c720251b1a8ca73a94fe50a
Gnarkill33 <gnarkill33@gmail.com>
2025-09-11 17:39:54 +0300

    feat: add translations for sign in form

--- src/components/SignInForm/SignInForm.tsx
+++ src/components/SignInForm/SignInForm.tsx
@@ -5,29 +5,31 @@ import styles from '@/components/SignUpForm/SignUpForm.module.css';
 import Button from '@/components/ui/Button/Button';
 
 export const SignInForm = () => {
-  const t = useTranslations('SignUp');
+  const t = useTranslations('SignIn');
   return (
     <form className={styles.form} noValidate>
       <Title className={styles.title} ta='center'>
-        {t('signUpTitle')}
+        {t('signInTitle')}
       </Title>
       <Stack gap={0}>
-        <TextInput className={styles.inputLabel} label='Email' required radius='md' id='email' />
+        <TextInput className={styles.inputLabel} label='Email' radius='md' id='email' />
         <p className={styles.formErrors}></p>
       </Stack>
 
       <Stack gap={0}>
-        <PasswordInput className={styles.inputLabel} label={t('enterPassword')} required radius='md' id='password' />
+        <PasswordInput className={styles.inputLabel} label={t('password')} radius='md' id='password' />
         <p className={styles.formErrors}></p>
       </Stack>
 
-      <Button className={styles.authBtn}>{t('signUpBtn')}</Button>
-      <Text ta='center' mt='sm' className={styles.subtitle}>
-        {t('signInInquiry')}{' '}
+      <Button className={styles.authBtn}>{t('signInBtn')}</Button>
+      <Stack gap={0}>
+        <Text ta='center' mt='sm' className={styles.subtitle}>
+          {t('signUpInquiry')}
+        </Text>
         <Anchor component='button' size='sm'>
-          {t('signInLink')}
+          {t('signUpLink')}
         </Anchor>
-      </Text>
+      </Stack>
     </form>
   );
 };
