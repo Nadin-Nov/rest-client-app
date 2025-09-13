6aed71a14e49d98d2f883cf7f6099b6fcf1cdb5e
Marta <emeraldraven27@gmail.com>
2025-09-13 13:09:57 +0300

    refactor: edit naming

--- src/components/ui/TextArea/TextArea.tsx
+++ src/components/ui/TextArea/TextArea.tsx
@@ -2,14 +2,14 @@ import type { FC, ChangeEvent } from 'react';
 
 import styles from './TextArea.module.css';
 
-interface MyTextareaProps {
+interface TextareaProps {
   value: string;
   onChange?: (value: string) => void;
   readOnly?: boolean;
   placeholder?: string;
 }
 
-export const TextArea: FC<MyTextareaProps> = ({ value, onChange, readOnly = false, placeholder = '' }) => {
+export const TextArea: FC<TextareaProps> = ({ value, onChange, readOnly = false, placeholder = '' }) => {
   const handleChange = (e: ChangeEvent<HTMLTextAreaElement>) => {
     if (onChange) onChange(e.currentTarget.value);
   };
