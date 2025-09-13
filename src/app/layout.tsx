7d720ea780a1e249d4acc383cc2d66f3503dfa76
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-09-13 17:06:17 +0300

    feat: change layout files

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,10 @@
-import { mantineHtmlProps } from '@mantine/core';
-import { Inter } from 'next/font/google';
-
-import '@mantine/core/styles.css';
 import './globals.css';
+import type { ReactNode } from 'react';
 
-import MantineProviderWrapper from '../providers/MantineProviderWrapper';
-
-const inter = Inter({ subsets: ['latin', 'cyrillic'], display: 'swap' });
-
-interface RootLayoutProps {
-  children: React.ReactNode;
-}
-
-export default function RootLayout({ children }: RootLayoutProps) {
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang='en' className={inter.className} {...mantineHtmlProps}>
-      <body>
-        <MantineProviderWrapper>{children}</MantineProviderWrapper>
-      </body>
+    <html lang='en'>
+      <body>{children}</body>
     </html>
   );
 }
