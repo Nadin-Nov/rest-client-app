aa559dd25c381143710deac6cb401cb31f5c163a
Gnarkill33 <gnarkill33@gmail.com>
2025-09-14 20:36:59 +0300

    fix: replace regular expressions with proper ones

--- src/validation.ts
+++ src/validation.ts
@@ -2,12 +2,12 @@ import * as z from 'zod';
 
 export const formSchema = z
   .object({
-    name: z.string().regex(/^[A-Z]/, 'Name should start with a capital letter'),
+    name: z.string().regex(/\p{Lu}/u, 'Name should start with a capital letter'),
     email: z.email('Email should have valid format'),
     password: z
       .string()
       .min(8, 'Password must be minimum 8 symbols long')
-      .regex(/[0-9]/, 'Password should contain 1 digit')
+      .regex(/\d/, 'Password should contain 1 digit')
       .regex(/\p{L}/u, 'Password should contain 1 letter')
       .regex(/[\W_]/, 'Password should contain 1 special character'),
     confirmPassword: z.string(),
