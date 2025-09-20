93908ee14ffec27dbc45ae0df7410896a92c7c8a
Nadin Novoselova <nadja.novoselova@gmail.com>
2025-09-20 14:32:15 +0300

    feat: add lazy losd for variables page

--- src/app/[locale]/variables/page.tsx
+++ src/app/[locale]/variables/page.tsx
@@ -1,18 +1,22 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { redirect } from 'next/navigation';
 
-import Variables from '@/components/Variables/VariablesComponent/Variables';
+import { PawSpinner } from '@/components/ui/PawSpinner/PawSpinner';
 import { useAuthContext } from '@/hooks/useAuthContext';
 
 import styles from './styles.module.css';
 
+const Variables = dynamic(() => import('@/components/Variables/VariablesComponent/Variables'), {
+  ssr: false,
+  loading: () => <PawSpinner />,
+});
+
 export default function VariablesPage() {
   const { isAuth } = useAuthContext();
 
-  if (!isAuth) {
-    redirect('/sign-in');
-  }
+  if (!isAuth) redirect('/sign-in');
 
   return (
     <div className={styles.container}>
